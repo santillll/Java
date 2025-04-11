8a507c3cad9f7fb907b2c0467dfdd9a9a1f4f4c4
santilll <santilenzo4@gmail.com>
2025-04-11 08:02:23 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,9 @@
-public class Ola{
-      public static void main(String[] args){
-        System.out.println("Boa tarde á todos!");
-    }    
+import javax.swing.JOptionPane;
+
+public class Ola {
+
+  public static void main(String[] args) {
+    //System.out.println("Boa tarde á todos!");
+    JOptionPane.showMessageDialog(null,"Boa tarde á todos!");
+  }
 }
